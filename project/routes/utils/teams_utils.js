6bdc5068c3a95d8343bec0a49f02979b4fddaf4f
daniel-izmaylov <dani.izmylov@gmail.com>
2021-05-21 17:13:23 +0300

    added Team func

--- project/routes/utils/teams_utils.js
+++ project/routes/utils/teams_utils.js
@@ -1,4 +1,4 @@
-const DButils = require("./DButils");
+const axios = require("axios");
 
 async function getTeamDetailsbyID(TEAM_ID) {
   const team = await axios.get(
@@ -13,10 +13,35 @@ async function getTeamDetailsbyID(TEAM_ID) {
   return {
     team_name: team.data.data.name,
     logo_path: team.data.data.logo_path,
+    coach_id: team.data.data.coach.data.coach_id,
     coach_name: team.data.data.coach.data.fullname,
-
-    current_stage_name: stage.data.data.namer,
+    trophies: team.data.data.trophies.data,
     // next game details should come from DB
   };
 }
-exports.getLeagueDetails = getLeagueDetails;
+
+async function getTeamDetailsbyName(TEAM_Name) {
+  const teams = await axios.get(
+    `https://soccer.sportmonks.com/api/v2.0/teams/search/${TEAM_Name}`,
+    {
+      params: {
+        api_token: process.env.api_token,
+      },
+    }
+  );
+  const Team_Info= [];
+  teams.data.data.forEach((element) => {
+    var obj= {
+      team_id: element.id,
+      team_name: element.name,
+      logo_path: element.logo_path,
+    }
+    Team_Info.push(obj)
+   } )
+
+  return Team_Info;
+}
+
+
+exports.getTeamDetailsbyID = getTeamDetailsbyID;
+exports.getTeamDetailsbyName = getTeamDetailsbyName;
