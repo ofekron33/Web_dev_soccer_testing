7533884404026cdcbe6bc613744500ab9ea67a32
dvirainisch <63093025+dvirainisch@users.noreply.github.com>
2021-06-10 10:26:23 +0300

    Update games_utils.test.js

--- project/tests/games_utils.test.js
+++ project/tests/games_utils.test.js
@@ -189,3 +189,126 @@ test('acceptence test getReferees', async () => {
       }
 
 });
+
+
+test('unittest schduleReffere', async () => {
+  let teams_matches_test = [
+      [
+        [
+          {
+            TeamId: 86,
+            Name: 'Silkeborg',
+            LeagueId: 271,
+            SeasonId: 18334,
+            Stadium: 'JYSK park',
+            Finance: null
+          },
+          {
+            TeamId: 85,
+            Name: 'København',
+            LeagueId: 271,
+            SeasonId: 18334,
+            Stadium: 'Parken',
+            Finance: null
+          }        ],
+        [
+          {
+            TeamId: 293,
+            Name: 'Brøndby',
+            LeagueId: 271,
+            SeasonId: 18334,
+            Stadium: 'Brøndby Stadion',
+            Finance: null
+          },
+          {
+            TeamId: 390,
+            Name: 'SønderjyskE',
+            LeagueId: 271,
+            SeasonId: 18334,
+            Stadium: 'Sydbank Park',
+            Finance: null
+          }        ],
+        [
+          {
+            TeamId: 939,
+            Name: 'Midtjylland',
+            LeagueId: 271,
+            SeasonId: 18334,
+            Stadium: 'MCH Arena',
+            Finance: null
+          },
+          {
+            TeamId: 1020,
+            Name: 'AaB',
+            LeagueId: 271,
+            SeasonId: 18334,
+            Stadium: 'Aalborg Portland Park',
+            Finance: null
+          }        ],
+        [
+          {
+            TeamId: 1789,
+            Name: 'OB',
+            LeagueId: 271,
+            SeasonId: 18334,
+            Stadium: 'Nature Energy Park',
+            Finance: null
+          },
+          {
+            TeamId: 2356,
+            Name: 'Randers',
+            LeagueId: 271,
+            SeasonId: 18334,
+            Stadium: 'Cepheus Park Randers',
+            Finance: null
+          }
+                ],
+        [
+          {
+            TeamId: 2394,
+            Name: 'Nordsjælland',
+            LeagueId: 271,
+            SeasonId: 18334,
+            Stadium: 'Right To Dream Park',
+            Finance: null
+          },
+          {
+            TeamId: 2447,
+            Name: 'Viborg',
+            LeagueId: 271,
+            SeasonId: 18334,
+            Stadium: 'Energi Viborg Arena',
+            Finance: null
+          }        ],
+        [
+          {
+            TeamId: 2905,
+            Name: 'AGF',
+            LeagueId: 271,
+            SeasonId: 18334,
+            Stadium: 'Ceres Park',
+            Finance: null
+          },
+          {
+            TeamId: 7466,
+            Name: 'Vejle',
+            LeagueId: 271,
+            SeasonId: 18334,
+            Stadium: 'Vejle Stadion',
+            Finance: null
+          }        ]
+      ]
+      ]
+  let res1 = true;
+  const matches1 = await games_utils.schduleReffere(teams_matches_test);
+  matches1.forEach((element1) => {
+    element1.forEach((match1) => {
+          let sub1 = match1.referee;
+          if(sub1 == null){
+            res1= false;
+          }
+
+      })
+     })
+  expect(res1).toBeTruthy();
+})
